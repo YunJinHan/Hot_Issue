ba5e94fe99416811a260d5435a84d7b340e98ce6
YunJinHan <yjh9234@naver.com>
2017-02-16 08:44:01 +0900

    Hot Issue

--- news/pipelines.py
+++ news/pipelines.py
@@ -3,7 +3,7 @@
 
 # Define your item pipelines here
 #
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# Don't forget to add your pipeline to the ITEM_PIPELINES settin
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import sys
