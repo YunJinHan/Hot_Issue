08f55e9bf9cf4c9ebebdd15bb214fc3b333d0ac4
YunJinHan <yjh9234@naver.com>
2017-02-15 15:26:08 +0900

    Hot Issue

---
+++ news/pipelines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import sys
+import MySQLdb
+import hashlib
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class NewsPipeline(object):
+
+    host = '127.0.0.1'
+    user = 'root'
+    password = '0000'
+    db = 'news'
+
+    def __init__(self):
+		self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db, charset="utf8", use_unicode=True)
+		self.cursor = self.connection.cursor()
+		self.cursor.execute("DELETE FROM article")
+
+    def process_item(self, item, spider):
+        try:
+            self.cursor.execute("INSERT INTO article VALUES (%s, %s)",(item['category'],item['title']))
+            self.connection.commit()
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+        return item
---
+++ news/spiders/newsSpider.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+import re
+from news.items import NewsItem
+from datetime import datetime
+
+class NewsSpider(scrapy.Spider):
+
+    name = "news"
+
+    def start_requests(self):
+        for i in range(0,8):
+            yield scrapy.Request("http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=10%d&listType=title" %i, self.parse_news)
+
+    def parse_news(self,response):
+
+        category = response.xpath('//*[@id="main_content"]/div[1]/h3/text()').extract()[0]
+
+        for sel in response.xpath('//*[@id="main_content"]/div[2]/ul/li'):
+            item = NewsItem()
+            item['category'] = category
+            item['title'] = sel.xpath('a/text()').extract()[0]
+            print item['category'] + " : " + item['title']
+            yield item
